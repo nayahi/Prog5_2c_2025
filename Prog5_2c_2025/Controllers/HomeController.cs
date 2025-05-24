96360ea1bbbbaff879e087e70f006f387de05a94
nayahi <nayahi@hotmail.com>
2025-05-24 10:31:01 -0600

    REQ-003 Calculadora basica

--- Prog5_2c_2025/Controllers/HomeController.cs
+++ Prog5_2c_2025/Controllers/HomeController.cs
@@ -44,5 +44,76 @@ namespace Prog5_2c_2025.Controllers
             return View("Suma2");
         }
         #endregion Sumar 2 numeros
+
+        #region Calculadora basica
+        public IActionResult bCalc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Suma()
+        {
+            try
+            {
+                int num1 = Convert.ToInt32(HttpContext.Request.Form["n1"].ToString());
+                int num2 = Convert.ToInt32(HttpContext.Request.Form["n2"].ToString());
+                ViewBag.Result = "Resultado de la suma: " + (num1 + num2).ToString();
+            }
+            catch (Exception)
+            {
+                ViewBag.Result = "Datos erroneos ingresados.";
+            }
+            return View("bCalc");
+        }
+
+        [HttpPost]
+        public IActionResult Resta()
+        {
+            try
+            {
+                int num1 = Convert.ToInt32(HttpContext.Request.Form["n1"].ToString());
+                int num2 = Convert.ToInt32(HttpContext.Request.Form["n2"].ToString());
+                ViewBag.Result = "Resultado de la resta: " + (num1 - num2).ToString();
+            }
+            catch (Exception)
+            {
+                ViewBag.Result = "Datos erroneos ingresados.";
+            }
+            return View("bCalc");
+        }
+        [HttpPost]
+        public IActionResult Multiplicacion()
+        {
+            try
+            {
+                int num1 = Convert.ToInt32(HttpContext.Request.Form["n1"].ToString());
+                int num2 = Convert.ToInt32(HttpContext.Request.Form["n2"].ToString());
+                ViewBag.Result = "Resultado de la multiplicación: " + (num1 * num2).ToString();
+            }
+            catch (Exception)
+            {
+                ViewBag.Result = "Datos erroneos ingresados.";
+            }
+            return View("bCalc");
+        }
+        [HttpPost]
+        public IActionResult Division()
+        {
+            try
+            {
+                decimal num1 = Convert.ToDecimal(HttpContext.Request.Form["n1"].ToString());
+                decimal num2 = Convert.ToDecimal(HttpContext.Request.Form["n2"].ToString());
+                decimal f = num1 / num2;
+                ViewBag.Result = "Resultado de la división: " + f.ToString();
+            }
+            catch (Exception)
+            {
+                ViewBag.Result = "Datos erroneos ingresados.";
+            }
+            return View("bCalc");
+        }
+
+        #endregion Calculadora basica
     }
 }
