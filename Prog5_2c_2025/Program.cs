b74a20dfd2d24f01539d43f779a72528c2a55433
nayahi <nayahi@hotmail.com>
2025-06-06 21:30:11 -0600

    PR-006 CRU de CentroVacunacion

--- Prog5_2c_2025/Program.cs
+++ Prog5_2c_2025/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Prog5_2c_2025.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
